716cf4ca640b678f9ea3c2d1e6c9a824ef24f778
ingcaranton <ing.caranton@gmail.com>
2014-10-16 07:33:35 -0500

    middleware que protege direcciones /users

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -4,7 +4,7 @@ var app = module.exports = express();
 app.set('views', __dirname + '/views');
 
 
-app.route('/cliente')
+app.route('/')
   .get(function(req, res) {
     
    res.render('cliente', {
@@ -14,3 +14,25 @@ app.route('/cliente')
       sesion: req.user
    });
   });
+
+  app.route('/*')
+  .get(isAuthenticated,function(req, res) {
+    
+   res.render('cliente', {
+      title: 'Cliente',
+      pCliente: 'active',
+      message: req.flash('message'),
+      sesion: req.user
+   });
+  });
+
+
+function isAuthenticated(req, res, next) {
+
+    if (req.user){
+        return next();
+    }
+
+     req.flash('message', 'No estas autenticado.');
+     res.redirect('/');
+}
